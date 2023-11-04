165b3b070918c23cec8a900bbf28b292332bd32b
Bublaaa <yanuarkrisna7@gmail.com>
2023-11-04 22:04:02 +0700

    [FE] Add size option in add menu

--- resources/views/layouts/contents/addmenu.blade.php
+++ resources/views/layouts/contents/addmenu.blade.php
@@ -51,17 +51,46 @@
                                 </span>
                                 @enderror
                             </div>
+                            <div class="form-group">
+                                <p>Ukuran</p>
+
+                                <label for="regular">
+                                    <input type="checkbox" id="regular" name="regularSize" value="1" checked>
+                                    Regular
+                                </label>
+
+                                <label for="large">
+                                    <input type="checkbox" id="large" name="largeSize">
+                                    Large
+                                </label>
+
+                                @error('regularSize')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                                @error('largeSize')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
                         </div>
+                    </div>
+                    <div class=" card-footer">
+                        <button class="btn btn-primary" type="submit">Simpan Menu</button>
                     </div>
                 </div>
             </div>
-            <!-- Ingredient Card -->
             <div class=" col-md-9">
+                <!-- Regular size ingredients card-->
                 <div class="card">
                     <div class="card-header">
                         <div class="row">
                             <div class="col-6">
-                                <h5>Bahan baku</h5>
+                                <h5>Bahan baku ukuran regular </h5>
                             </div>
                             <div class="col-6 text-right">
                                 <button name="addIngredient" id="addIngredient" type="button" class="btn btn-primary">
@@ -73,20 +102,20 @@
                     <div class="card-body">
                         <div class="container" id="ingredientContainer">
                             <div class="row">
-                                <div class="col-3">
+                                <div class="col-4 col-md-4">
                                     <p>Nama</p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-4 col-md-3">
                                     <p>Jumlah</p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-4 col-md-3">
                                     <p>Satuan</p>
                                 </div>
-                                <div class="col-3">
+                                <div class="col-2 col-md-2">
                                 </div>
                             </div>
                             <div class="row ingredient-row">
-                                <div class="col col-md">
+                                <div class="col-4 col-md-4">
                                     <div class="form-group">
                                         <input type="text" name="ingredients[0][name]"
                                             class="form-control @error('ingredients[0][name]') is-invalid @enderror"
@@ -99,7 +128,7 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col">
+                                <div class="col-4 col-md-3">
                                     <div class="form-group">
                                         <input type="number" name="ingredients[0][quantity]"
                                             class="form-control @error('ingredients[0][quantity]') is-invalid @enderror"
@@ -113,7 +142,7 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col">
+                                <div class="col-4 col-md-3">
                                     <div class="form-group">
                                         <select name="ingredients[0][unit]" class="form-control"
                                             id="ingredients[0][unit]">
@@ -127,7 +156,7 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col">
+                                <div class="col-1 col-md-2">
                                     <button type="button" class="btn btn-danger remove-row" onclick="removeRow(this)">
                                         <i class="fas fa-trash"></i>
                                     </button>
@@ -135,16 +164,115 @@
                             </div>
                         </div>
                     </div>
-                    <div class=" card-footer">
-                        <button class="btn btn-primary" type="submit">Simpan Menu</button>
+
+                </div>
+                <!-- Large size ingredients card -->
+                <div class="card" id="largeIngredientsCard" style="display:none;">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>Bahan baku ukuran besar</h5>
+                            </div>
+                            <div class="col-6 text-right">
+                                <button name="addIngredientLarge" id="addIngredientLarge" type="button"
+                                    class="btn btn-primary">
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="container" id="largeIngredientContainer">
+                            <div class="row">
+                                <div class="col-4 col-md-4">
+                                    <p>Nama</p>
+                                </div>
+                                <div class="col-4 col-md-3">
+                                    <p>Jumlah</p>
+                                </div>
+                                <div class="col-4 col-md-3">
+                                    <p>Satuan</p>
+                                </div>
+                                <div class="col-2 col-md-2">
+                                </div>
+                            </div>
+                            <div class="row ingredient-row">
+                                <div class="col-4 col-md-4">
+                                    <div class="form-group">
+                                        <input type="text" name="largeIngredients[0][name]"
+                                            class="form-control @error('largeIngredients[0][name]') is-invalid @enderror"
+                                            id="largeIngredients[0][name]" placeholder="Nama Bahan"
+                                            value="{{ old('largeIngredients[0][name]') }}" required=true>
+                                        @error('largeIngredients[0][name]')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-4 col-md-3">
+                                    <div class="form-group">
+                                        <input type="number" name="largeIngredients[0][quantity]"
+                                            class="form-control @error('largeIngredients[0][quantity]') is-invalid @enderror"
+                                            id="largeIngredients[0][quantity]" placeholder="Jumlah Bahan"
+                                            value="{{ old('largeIngredients[0][quantity]') }}" inputmode="numeric"
+                                            min="1" required=true>
+                                        @error('largeIngredients[0][quantity]')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-4 col-md-3">
+                                    <div class="form-group">
+                                        <select name="largeIngredients[0][unit]" class="form-control"
+                                            id="largeIngredients[0][unit]">
+                                            <option value="gram">Gram</option>
+                                            <option value="ml"> Mililiter</option>
+                                        </select>
+                                        @error('unit')
+                                        <span class="largeIngredients[0][unit]" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-1 col-md-2">
+                                    <button type="button" class="btn btn-danger remove-row-large"
+                                        onclick="removeRow(this)">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </form>
 </div>
-
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script>
+$(document).ready(function() {
+    // Large checkbox toggle
+    $('#largeIngredientsCard').css('display', $('#large').prop('checked') ? 'block' : 'none');
+
+    $('#large').on('change', function() {
+        $('#largeIngredientsCard').css('display', $(this).prop('checked') ? 'block' : 'none');
+    });
+
+    // Prevent regular option to be unchecked
+    $('#regular').prop('checked', true);
+
+    $('#regular').on('change', function() {
+        if (!$(this).prop('checked')) {
+            alert('Ukuran reguler tidak bisa diubah.');
+            $(this).prop('checked', true);
+        }
+    });
+});
+
 // Prevent the last remaining row from deleted & delete selected row
 function removeRow(button) {
     var row = button.closest(".row");
@@ -161,63 +289,132 @@ document.getElementById("addIngredient").onclick = function() {
     ++ingredientIndex;
 
     var newRow = document.createElement("div");
-    newRow.className = "row";
+    newRow.className = "row py-2";
     newRow.innerHTML = `
-            <div class="col">
-                <div class="form-group">
-                    <div class="form-group">
-                        <input type="text" name="ingredients[${ingredientIndex}][name]"
-                            class="form-control @error('ingredients[${ingredientIndex}][name]') is-invalid @enderror"
-                            id="ingredients[${ingredientIndex}][name]" placeholder="Nama Bahan"
-                            value="{{ old('ingredients[${ingredientIndex}][name]') }}">
-                        @error('ingredients[${ingredientIndex}][name]')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <div class="form-group">
-                        <input type="text" name="ingredients[${ingredientIndex}][quantity]"
-                            class="form-control @error('ingredients[${ingredientIndex}][quantity]') is-invalid @enderror"
-                            id="ingredients[${ingredientIndex}][quantity]" placeholder="Jumlah Bahan"
-                            value="{{ old('ingredients[${ingredientIndex}][quantity]') }}">
-                        @error('ingredients[${ingredientIndex}][quantity]')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <select name="ingredients[${ingredientIndex}][unit]" class="form-control" id="ingredients[${ingredientIndex}][unit]">
-                        <option value="gram">Gram</option>
-                        <option value="ml"> Mililiter</option>
-                    </select>
-                    @error('unit')
-                    <span class="ingredients[${ingredientIndex}][unit]" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-            </div>
-            <div class="col">
-                <button type="button" class="btn btn-danger remove-row" onclick="removeRow(this)">
-                    <i class="fas fa-trash"></i>
-                </button>
-            </div>
-        `;
+<div class="col-4 col-md-4">
+    <div class="form-group">
+        <div class="form-group">
+            <input type="text" name="ingredients[${ingredientIndex}][name]"
+                class="form-control @error('ingredients[${ingredientIndex}][name]') is-invalid @enderror"
+                id="ingredients[${ingredientIndex}][name]" placeholder="Nama Bahan"
+                value="{{ old('ingredients[${ingredientIndex}][name]') }}">
+            @error('ingredients[${ingredientIndex}][name]')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+<div class="col-4 col-md-3">
+    <div class="form-group">
+        <div class="form-group">
+            <input type="text" name="ingredients[${ingredientIndex}][quantity]"
+                class="form-control @error('ingredients[${ingredientIndex}][quantity]') is-invalid @enderror"
+                id="ingredients[${ingredientIndex}][quantity]" placeholder="Jumlah Bahan"
+                value="{{ old('ingredients[${ingredientIndex}][quantity]') }}">
+            @error('ingredients[${ingredientIndex}][quantity]')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+<div class="col-4 col-md-3">
+    <div class="form-group">
+        <select name="ingredients[${ingredientIndex}][unit]" class="form-control"
+            id="ingredients[${ingredientIndex}][unit]">
+            <option value="gram">Gram</option>
+            <option value="ml"> Mililiter</option>
+        </select>
+        @error('unit')
+        <span class="ingredients[${ingredientIndex}][unit]" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+</div>
+<div class="col-12 col-md-2">
+    <button type="button" class="btn btn-danger remove-row" onclick="removeRow(this)">
+        <i class="fas fa-trash"></i>
+    </button>
+</div>
+`;
 
     // Append the new row to the container
     document.getElementById("ingredientContainer").appendChild(newRow);
 
     // Attach the onclick event to the remove button in the new row
     newRow.querySelector(".remove-row").onclick = function() {
+        removeRow(this);
+    };
+};
+
+// Add new row when button clicked - LARGE
+var ingredientLargeIndex = 0;
+document.getElementById("addIngredientLarge").onclick = function() {
+    ++ingredientLargeIndex;
+
+    var newRow = document.createElement("div");
+    newRow.className = "row py-2";
+    newRow.innerHTML = `
+<div class="col-4 col-md-4">
+    <div class="form-group">
+        <div class="form-group">
+            <input type="text" name="largeIngredients[${ingredientLargeIndex}][name]"
+                class="form-control @error('largeIngredients[${ingredientLargeIndex}][name]') is-invalid @enderror"
+                id="largeIngredients[${ingredientLargeIndex}][name]" placeholder="Nama Bahan"
+                value="{{ old('largeIngredients[${ingredientLargeIndex}][name]') }}">
+            @error('largeIngredients[${ingredientLargeIndex}][name]')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+<div class="col-4 col-md-3">
+    <div class="form-group">
+        <div class="form-group">
+            <input type="text" name="largeIngredients[${ingredientLargeIndex}][quantity]"
+                class="form-control @error('largeIngredients[${ingredientLargeIndex}][quantity]') is-invalid @enderror"
+                id="largeIngredients[${ingredientLargeIndex}][quantity]" placeholder="Jumlah Bahan"
+                value="{{ old('largeIngredients[${ingredientLargeIndex}][quantity]') }}">
+            @error('largeIngredients[${ingredientLargeIndex}][quantity]')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+<div class="col-4 col-md-3">
+    <div class="form-group">
+        <select name="largeIngredients[${ingredientLargeIndex}][unit]" class="form-control"
+            id="largeIngredients[${ingredientLargeIndex}][unit]">
+            <option value="gram">Gram</option>
+            <option value="ml"> Mililiter</option>
+        </select>
+        @error('unit')
+        <span class="largeIngredients[${ingredientLargeIndex}][unit]" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+</div>
+<div class="col-12 col-md-2">
+    <button type="button" class="btn btn-danger remove-row-large" onclick="removeRow(this)">
+        <i class="fas fa-trash"></i>
+    </button>
+</div>
+`;
+
+    // Append the new row to the container
+    document.getElementById("largeIngredientContainer").appendChild(newRow);
+
+    // Attach the onclick event to the remove button in the new row
+    newRow.querySelector(".remove-row-large").onclick = function() {
         removeRow(this);
     };
 };
