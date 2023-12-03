255d07e40b2592a3f15b6feeaa1c3dc93c07a608
Bublaaa <yanuarkrisna7@gmail.com>
2023-12-03 20:32:22 +0700

    Add boosttrap to edituserBalde

--- resources/views/layouts/contents/editUser.blade.php
+++ resources/views/layouts/contents/editUser.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.ownerView')
 
 @section('content')
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    </script>
+</head>
 <h2>Edit Akun Karyawan</h2>
 <div class="card">
     <div class="card-body">
